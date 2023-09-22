5af9ca6c3e04f7a75cc5f0495eb6c841b9037447
Joost <joost@vanewijk.info>
2023-09-22 15:24:25 +0200

    Footer + nav af

--- script/scroll.js
+++ script/scroll.js
@@ -1,19 +1,18 @@
+// Define the topFunction function in the global scope
+function topFunction() {
+    const scrollToTop = () => {
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+            window.requestAnimationFrame(scrollToTop);
+            window.scrollTo(0, c - c / 10); // You can adjust the division value to control the animation speed
+        }
+    };
+    scrollToTop();
+}
+
 // Get the button:
 let mybutton = document.getElementById("UpBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// Attach the topFunction to the button's click event
+mybutton.addEventListener("click", topFunction);
