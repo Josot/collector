eaa48ae6e6845ce503148adb944065098879e59a
Josot <joost@vanewijk.info>
2023-10-29 17:44:00 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moto Garage</title>
+
+    <link rel="stylesheet" href="./css/stylesheet.css">
+    <link rel="stylesheet" href="./css/responsive.css">
+    <link rel="icon" href="./media/logo/logo2.png">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+</head>
+
+<body>
+    <main>
+        <header id="header" style="background-color: #222;">
+
+            <!-- NAVBAR -->
+            <nav>
+                <div class="container">
+                    <div class="logo">
+                        <a href="./"><img class="main-logo" src="./media/logo/logo2.png" alt=""></a>
+                    </div>
+                    <div class="links">
+                        <ul>
+                            <li>
+                                <a href="./about-us/">About us</a>
+                            </li>
+                            <li>
+                                <a href="./inventory/">Inventory</a>
+                            </li>
+                            <li>
+                                <a href="#" class="active">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="hamburger-menu">
+                        <div class="bar"></div>
+                    </div>
+                </div>
+            </nav>
+            <!-- NAVBAR -->
+
+
+            <div class="header-content">
+                <video class="header-video" src="./media/video/homepage_video.mp4" autoplay muted loop></video>
+
+                <div class="area">
+                    <div class="container grid-2">
+                        <div class="column-1">
+                            <h1 class="header-title" style="color: white;">Moto Garage</h1>
+                            <p class="text" style="color: white;">
+                                We are Moto Garage, we are a place of love for motorcycles since 1954, ready to give you the best deals.
+                                We sell you your favourite motorcycles for a low price and buy your motorcycles aswell.
+                                We are happy to be of service!
+                            </p>
+                            <a href="./inventory/" class="btn">Look at our inventory!</a>
+                            <br><br>
+                            <a href="./about-us/index.php" class="btn">About us!</a>
+                        </div>
+
+                        <div class="column-2 image">
+                            <img class="img-logo-header" src="./media/logo/logo2.png" alt="">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </header>
+
+
+    </main>
+    <footer class="footer">
+        <div class="container">
+            <div class="grid-4">
+                <div class="grid-4-col footer-about">
+                    <h3 class="title-sm">About "Moto Garage"</h3>
+                    <p class="text">
+                    We are Moto Garage, we are a place of love for motorcycles since 1954, ready to give you the best deals.
+                    We sell you your favourite motorcycles for a low price and buy your motorcycles aswell.
+                    We are happy to be of service!
+                    </p>
+                </div>
+
+                <div class="grid-4-col footer-links">
+                    <h3 class="title-sm">Links</h3>
+                    <ul>
+                        <li>
+                            <a href="./inventory/">Inventory</a>
+                        </li>
+                        <li>
+                            <a href="./about-us/">About us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="grid-4-col footer-links">
+                    <h3 class="title-sm">What We do Here</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Sell motorcycles</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="bottom-footer">
+                <div class="copyright">
+                    <p class="text">
+                        Copyright&copy;2023 All rights reserved by
+                        <span>Moto Garage</span>
+                    </p>
+                </div>
+
+                <div class="followme-wrap">
+                    <div class="followme">
+                        <h3>Follow Creator</h3>
+                        <span class="footer-line"></span>
+                        <div class="social-media">
+                            <a href="https://github.com/Josot" target="_blank">
+                                <i class="fab fa-github"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/joost-van-ewijk/" target="_blank">
+                                <i class="fab fa-linkedin"></i>
+                            </a>
+                            <a href="https://twitter.com/KidYeh" target="_blank">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+
+                        </div>
+                    </div>
+
+                    <div class="back-btn-wrap">
+                        <a href="#" class="back-btn">
+                            <i class="fas fa-chevron-up"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <script src="./javascript/hamburgermenu.js"></script>
+
+</body>
+
+</html>
